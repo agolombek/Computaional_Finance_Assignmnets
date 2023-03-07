9361c3c6f35e716a9a488ec446394dbf250bc07a
agolombek <aron.golombek@student.uva.nl>
2023-03-07 13:50:23 +0100

    plots

--- Assignment_2/Digital_Option.py
+++ Assignment_2/Digital_Option.py
@@ -27,10 +27,7 @@ def DigitalMonteCarlo(S0, r, T, K, sigma, n_sim, Z):
     
     option_value = maturity_payoff*np.exp(-r*T)
 
-    avg_val = np.mean(option_value)
-    std_val = np.std(option_value)
-    
-    return avg_val, std_val
+    return option_value
 
 
 def AnalyticalDigitalDelta(S, K, r, T, vol):
@@ -43,6 +40,35 @@ def AnalyticalDigitalDelta(S, K, r, T, vol):
     
     delta_0 = np.exp(-r*T)*N_d2/(S*sigma*np.sqrt(T))
     return delta_0
+
+
+def STD_FDM(set_1, set_2, bump, n_sim):
+    
+    cov_matrix = np.cov(np.stack((set_1, set_2), axis=0))
+    
+    var_1 = cov_matrix[0][0]
+    var_2 = cov_matrix[1][1]
+
+    cov = cov_matrix[0][1]
+    
+    total_SE = np.sqrt((var_1 + var_2 + 2*cov)/np.square(bump))/np.sqrt(n_sim)
+    
+    return total_SE
+
+
+def STD_CDM(set_1, set_2, bump, n_sim):
+    
+    cov_matrix = np.cov(np.stack((set_1, set_2), axis=0))
+    
+    var_1 = cov_matrix[0][0]
+    var_2 = cov_matrix[1][1]
+    
+    cov = cov_matrix[0][1]
+    
+    total_SE = np.sqrt((var_1 + var_2 + 2*cov)/np.square(2*bump))/np.sqrt(n_sim)
+    
+    return total_SE
+
 
 sigma = 0.2
 S0 = 100
@@ -61,10 +87,14 @@ analytical = AnalyticalDigitalDelta(S0, K, r, T, sigma)*np.ones(len(bumps))
 seed = 0
 np.random.seed(seed)
 Z  = np.random.normal(size=n_sim)
-base_value, std = DigitalMonteCarlo(S0, r, T, K, sigma, n_sim, Z)
+base_value = DigitalMonteCarlo(S0, r, T, K, sigma, n_sim, Z)
+base_avg = np.mean(base_value)
 
 FDM_delta = np.zeros(len(bumps))
+FDM_std = np.zeros(len(bumps))
+
 CDM_delta = np.zeros(len(bumps))
+CDM_std = np.zeros(len(bumps))
 
 
 i = 0
@@ -73,28 +103,54 @@ for h in bumps:
     S_f = S0 + S0*h
     S_b = S0 - S0*h
 
-    bumped_value, std_forward = DigitalMonteCarlo(S_f, r, T, K, sigma, n_sim, Z)
-    backward_bump_value, std_back = DigitalMonteCarlo(S_b, r, T, K, sigma, n_sim, Z)
+    bumped_values = DigitalMonteCarlo(S_f, r, T, K, sigma, n_sim, Z)
+    bump_avg = np.mean(bumped_values)
     
-    delta_f = (bumped_value-base_value)/(h*S0)
+    backward_bump_values= DigitalMonteCarlo(S_b, r, T, K, sigma, n_sim, Z)
+    bk_bump_avg = np.mean(backward_bump_values)
+    
+    
+    delta_f = (bump_avg-base_avg)/(h*S0)
     FDM_delta[i] = delta_f
+    FDM_std[i] = STD_FDM(base_value, bumped_values, S0*h, n_sim)
     
-    delta_c = (bumped_value-backward_bump_value)/(2*h*S0)
+    delta_c = (bump_avg-bk_bump_avg)/(2*h*S0)
     CDM_delta[i] = delta_c
+    CDM_std[i] = STD_CDM(bumped_values, backward_bump_values, S0*h, n_sim)
+    
     i+=1
 
 plt.plot(bumps*100, analytical, color='black', label='Analytical', linestyle='dashed')
+
 plt.plot(bumps*100, FDM_delta, color='C0', label='Forward Difference')
+# plt.fill_between(bumps*100, FDM_delta + FDM_std, FDM_delta - FDM_std, color='C0', alpha = 0.5)
+
 plt.plot(bumps*100, CDM_delta, color='green', label='Central Difference')
-plt.xlabel('h [%]',fontsize=12)
-plt.ylabel(r'$\Delta_{0}$',fontsize=12)
-plt.legend()
+# plt.fill_between(bumps*100, CDM_delta + CDM_std, CDM_delta - CDM_std, color='green', alpha = 0.5)
+
+plt.xlabel('h [%]',fontsize=16)
+plt.ylabel(r'$\Delta_{0}$',fontsize=16)
+plt.legend(fontsize=14)
 plt.xscale('log')
-# plt.xticks(fontsize=12)
-# plt.yticks(fontsize=12)
+plt.xticks(fontsize=16)
+plt.yticks(fontsize=16)
 plt.grid()
 plt.tight_layout()
-# plt.savefig('digital_option_delta.pdf', format="pdf")
+# plt.savefig('digital_bump_delta_same_seed.pdf', format="pdf")
+plt.show()
+
+plt.plot(bumps*100, FDM_std, color='C0', label='Forward Difference')
+plt.plot(bumps*100, CDM_std, color='green', label='Central Difference')
+plt.xlabel('h [%]',fontsize=16)
+plt.ylabel('Standrad Error',fontsize=16)
+plt.legend(fontsize=14)
+plt.grid()
+plt.xscale('log')
+plt.yscale('log')
+plt.xticks(fontsize=16)
+plt.yticks(fontsize=16)
+plt.tight_layout()
+# plt.savefig('digital_bump_SE_same_seed.pdf', format="pdf")
 plt.show()
 
 ####################### PathwiseDerivative Estimate ###########################
